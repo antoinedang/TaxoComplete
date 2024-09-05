33336d293aed4a4f5e9152449025a0c04824fbb8
Antoine Dangeard <antoine.dangeard@gmail.com>
2024-09-05 10:58:10 -0400

    make job tracker better

--- track_job.sh
+++ track_job.sh
@@ -1,3 +1,3 @@
 job_id=$(cat sbatch_out.txt | awk '{print $4}')
 
-watch scontrol show job $job_id
+watch -n 0.1 "echo =======STDOUT======= && (tail job_output.txt || true) && echo '' && echo =======STDERR======= && tail job_error.txt && echo '' && echo =======STATUS======= && scontrol show job $job_id"
