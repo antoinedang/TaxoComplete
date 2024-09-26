ffac96245b65dfea294c14161de3da79a0be4a16
Antoine Dangeard <antoine.dangeard@gmail.com>
2024-09-26 13:20:56 -0400

    clean up

--- track_job.sh
+++ track_job.sh
@@ -1,3 +1,1 @@
-job_id=$(cat sbatch_out.txt | awk '{print $4}')
-
-watch scontrol show job $job_id
+watch -n 0.1 "echo =======STDOUT======= && (tail job_output.txt || true) && echo '' && echo =======STDERR======= && (tail job_error.txt || true) && echo '' && echo =======STATUS======= && scontrol show job $(cat sbatch_out.txt | awk '{print $4}')"
