063dd2062e5b8e5260e842309ca5b55d9b0c9622
Antoine Dangeard <antoine.dangeard@gmail.com>
2024-11-21 19:57:09 -0500

    plotted graph distances and labels for psy dataset

--- src/plot_distances.py
+++ src/plot_distances.py
@@ -4,6 +4,17 @@ import pickle
 import os
 import matplotlib.pyplot as plt
 import networkx as nx
+import io
+import numpy as np
+
+
+class CPU_Unpickler(pickle.Unpickler):
+    def find_class(self, module, name):
+        if module == "torch.storage" and name == "_load_from_bytes":
+            return lambda b: torch.load(io.BytesIO(b), map_location="cpu")
+        else:
+            return super().find_class(module, name)
+
 
 torch.manual_seed(0)
 args = argparse.ArgumentParser(description="")
@@ -14,7 +25,7 @@ args.add_argument(
 )
 args = args.parse_args()
 
-error_analysis_dir = os.path.dirname(args.filename)
+error_analysis_dir = os.path.dirname(os.path.dirname(args.filename))
 plot_filename = error_analysis_dir + "/graph_distance_distributions.png"
 
 with open(args.filename, "rb") as f:
@@ -30,35 +41,51 @@ with open(args.filename, "rb") as f:
         corpus_embeddings,
         nodeId2corpusId,
         preds,
-    ) = pickle.load(f)
+    ) = CPU_Unpickler(f).load()
 
 core_subgraph_undirected = data_prep.core_subgraph.to_undirected()
 core_subgraph_undirected.remove_node(data_prep.pseudo_leaf_node)
 
 # get distances between all pairs of nodes in the graph
+print("Computing graph distances...")
+all_distances = dict(nx.all_pairs_shortest_path_length(core_subgraph_undirected))
 distances = []
-distances = nx.all_pairs_shortest_path_length(core_subgraph_undirected)
+test_query_node_ids = [data_prep.corpusId2nodeId[i] for i in range(len(data_prep.test_queries))]
+for i in range(len(data_prep.test_queries)):
+    print(f"Query {i+1}/{len(data_prep.test_queries)}")
+    for n in core_subgraph_undirected.nodes:
+        if n != data_prep.corpusId2nodeId[i]:
+            distances.append(all_distances[data_prep.corpusId2nodeId[i]][n])
 
 # MAKE A SCATTER PLOT OF THE DISTANCES
 plt.figure(figsize=(10, 6))
-plt.hist(distances, bins=100)
+plt.hist(distances, bins=50)
 # change x axis range
-plt.xlim(-1, 1)
 plt.xlabel("Graph Distance")
 plt.ylabel("# Node Pairs")
 plt.title("Pairwise distances between nodes in taxonomy")
 plt.savefig(plot_filename)
 
+def scaled_label(x, lower, upper):
+    return (upper - lower) * (1/x) + lower
 
-label_plot_filename = error_analysis_dir + "/label_distributions.png"
-labeling_fn = lambda d: (1 / d) * (0.5625 + 0.1875) - 0.1875
-labels = [labeling_fn(d) for d in distances]
-
-plt.figure(figsize=(10, 6))
-plt.hist(labels, bins=100)
-# change x axis range
-plt.xlim(-1, 1)
-plt.xlabel("Label")
-plt.ylabel("# Node Pairs")
-plt.title("Labels between nodes in taxonomy")
-plt.savefig(plot_filename)
+for label_name, labeling_fn in [("original", lambda d : 1/d),
+                                ("epsilon", lambda d : 1/(d+0.1)),
+                                ("linear", lambda d : max(-0.9, 1 - (0.1*d))),
+                                ("boundary", lambda d : scaled_label(d, 0, 0.9)),
+                                ("full_boundary", lambda d : scaled_label(d, -0.9, 0.9)),
+                                ("min_max", lambda d : scaled_label(d, -0.1875, 0.5625)),
+                                ]:
+    label_plot_filename = error_analysis_dir + f"/{label_name}_label_distributions.png"
+    print(f"Computing {label_name} labels...")
+    labels = [labeling_fn(d) for d in distances]
+    # labels.append(labeling_fn(min(distances)-0.9))
+    # labels.append(labeling_fn(max(distances)+1))
+    plt.figure(figsize=(10, 6))
+    plt.hist(labels, bins=50)
+    # change x axis range
+    plt.xlim(-1, 1)
+    plt.xlabel("Label")
+    plt.ylabel("# Node Pairs")
+    plt.title(f"Labels between nodes in taxonomy ({label_name} label fn)")
+    plt.savefig(label_plot_filename)
