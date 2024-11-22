e86558d50dc2e4dbb8ac1e56cb58a9cd4d47f7f4
antoinedang <antoine.dangeard@gmail.com>
2024-11-22 17:10:00 -0500

    debug script

--- check_experiment_status.sh
+++ check_experiment_status.sh
@@ -1,18 +1,17 @@
-experiments_dir=$1
+experiments_dir="$HOME/experiments"
 
 experiments=$(ls "$experiments_dir")
 
 cd "$experiments_dir"
 
 for experiment in $experiments; do
-  cd "$experiment"
+	cd "$experiment"
 
-  status=$(scontrol show job $(cat sbatch_out.txt | awk '{print $4}') 2> /dev/null | grep JobState | awk -F'=' '{for(i=1;i<=NF;i++) if($i ~ /JobState/) {split($(i+1), arr, " "); print arr[1]; break}}')
-  if [ -z $status ]; then
-	status="DONE"
-  fi
-  echo "$experiment   ->   $status"
-  cd ..
+	status=$(scontrol show job $(cat sbatch_out.txt | awk '{print $4}') 2> /dev/null | grep JobState | awk -F'=' '{for(i=1;i<=NF;i++) if($i ~ /JobState/) {split($(i+1), arr, " "); print arr[1]; break}}')
+	if [ -z $status ]; then
+		status="DONE"
+	fi
+	echo "$experiment   ->   $status"
+	cd ..
 done
-
 cd $HOME/TaxoComplete
