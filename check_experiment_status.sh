360f354b6aada9a6b79578b6a483f9f088d85040
antoinedang <antoine.dangeard@gmail.com>
2024-11-22 16:54:06 -0500

    fix bug in script

--- check_experiment_status.sh
+++ check_experiment_status.sh
@@ -6,10 +6,13 @@ cd "$experiments_dir"
 
 for experiment in $experiments; do
   cd "$experiment"
-  status=$(scontrol show job $(cat sbatch_out.txt | awk '{print $4}') | grep JobState | awk -F'=' '{for(i=1;i<=NF;i++) if($i ~ /JobState/) {split($(i+1), arr, " "); print arr[1]; break}}')
+
+  status=$(scontrol show job $(cat sbatch_out.txt | awk '{print $4}') 2> /dev/null | grep JobState | awk -F'=' '{for(i=1;i<=NF;i++) if($i ~ /JobState/) {split($(i+1), arr, " "); print arr[1]; break}}')
+  if [ -z $status ]; then
+	status="DONE"
+  fi
   echo "$experiment   ->   $status"
   cd ..
-  
 done
 
 cd $HOME/TaxoComplete
