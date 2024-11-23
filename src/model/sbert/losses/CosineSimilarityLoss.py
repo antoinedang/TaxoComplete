b4f4181b2389a3537adb6d31547c82eb6636bd9f
Antoine Dangeard <antoine.dangeard@gmail.com>
2024-11-22 19:13:57 -0500

    fix

--- src/model/sbert/losses/CosineSimilarityLoss.py
+++ src/model/sbert/losses/CosineSimilarityLoss.py
@@ -60,7 +60,8 @@ class CosineSimilarityLoss(nn.Module):
                 query_embedding, corpus_embedding
             )
             return self.loss_fct(
-                -2 * query_corpus_cossim + 3, labels[:, 0].view(-1)
+                float(-2) * query_corpus_cossim + float(3),
+                labels[:, 0].view(-1).float(),
             ).float()
         else:
             query_corpus_loss = torch.cosine_similarity(
