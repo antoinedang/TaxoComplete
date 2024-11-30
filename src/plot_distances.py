3428165b6f8bcca6757685194352fc1e697465f8
Antoine Dangeard <antoine.dangeard@gmail.com>
2024-11-30 16:31:15 -0500

    new plots

--- src/plot_distances.py
+++ src/plot_distances.py
@@ -50,7 +50,9 @@ core_subgraph_undirected.remove_node(data_prep.pseudo_leaf_node)
 print("Computing graph distances...")
 all_distances = dict(nx.all_pairs_shortest_path_length(core_subgraph_undirected))
 distances = []
-test_query_node_ids = [data_prep.corpusId2nodeId[i] for i in range(len(data_prep.test_queries))]
+test_query_node_ids = [
+    data_prep.corpusId2nodeId[i] for i in range(len(data_prep.test_queries))
+]
 for i in range(len(data_prep.test_queries)):
     print(f"Query {i+1}/{len(data_prep.test_queries)}")
     for n in core_subgraph_undirected.nodes:
@@ -66,17 +68,33 @@ plt.ylabel("# Node Pairs")
 plt.title("Pairwise distances between nodes in taxonomy")
 plt.savefig(plot_filename)
 
-def scaled_label(x, lower, upper):
-    return (upper - lower) * (1/x) + lower
 
-for label_name, labeling_fn in [("original", lambda d : 1/d),
-                                ("epsilon", lambda d : 1/(d+0.1)),
-                                ("linear", lambda d : max(-0.9, 1 - (0.1*d))),
-                                ("boundary", lambda d : scaled_label(d, 0, 0.9)),
-                                ("full_boundary", lambda d : scaled_label(d, -0.9, 0.9)),
-                                ("min_max", lambda d : scaled_label(d, -0.1875, 0.5625)),
-                                ("min_max_mistake", lambda d : scaled_label(d, 0.1875, 0.9375)),
-                                ]:
+def scaled_label(x, lower, upper):
+    return (upper - lower) * (1 / x) + lower
+
+
+max_graph_distance = nx.diameter(core_subgraph_undirected)
+min_graph_distance = 1
+
+
+def linear_scaled_label(x, lower, upper):
+    label_to_assign = (max_graph_distance - x) / (
+        max_graph_distance - min_graph_distance
+    )
+    label_to_assign = label_to_assign * (upper - lower) + lower
+    return label_to_assign
+
+
+for label_name, labeling_fn in [
+    ("original", lambda d: 1 / d),
+    ("epsilon", lambda d: 1 / (d + 0.1)),
+    ("linear", lambda d: max(-0.9, 1 - (0.1 * d))),
+    ("boundary", lambda d: scaled_label(d, 0, 0.9)),
+    ("full_boundary", lambda d: scaled_label(d, -0.9, 0.9)),
+    ("min_max", lambda d: scaled_label(d, -0.1875, 0.5625)),
+    ("min_max_mistake", lambda d: scaled_label(d, 0.1875, 0.9375)),
+    ("linear_scale", lambda d: linear_scaled_label(d, -0.1875, 0.5625)),
+]:
     label_plot_filename = error_analysis_dir + f"/{label_name}_label_distributions.png"
     print(f"Computing {label_name} labels...")
     labels = [labeling_fn(d) for d in distances]
@@ -88,5 +106,7 @@ for label_name, labeling_fn in [("original", lambda d : 1/d),
     plt.xlim(-1, 1)
     plt.xlabel("Label")
     plt.ylabel("# Node Pairs")
+    # make y axis log scale
+    plt.yscale("log")
     plt.title(f"Labels between nodes in taxonomy ({label_name} label fn)")
     plt.savefig(label_plot_filename)
