bd22bf505059be5bd32af8f5ac22dc3a3edde95c
Antoine Dangeard <antoine.dangeard@gmail.com>
2024-12-06 19:09:05 -0500

    cosine similarity fix

--- src/data_process/helpers.py
+++ src/data_process/helpers.py
@@ -5,6 +5,7 @@ from numbers import Number
 from typing import Union
 from pathlib import Path
 import scipy.sparse as sp
+from sentence_transformers import util
 
 data_dir = Path(__file__).parent
 
@@ -17,11 +18,7 @@ def set_seed(seed):
 
 
 def compute_cosine_ranges(range_percentile, query_embeddings, node_embeddings):
-    cosine_similarities = (
-        torch.nn.functional.cosine_similarity(query_embeddings, node_embeddings)
-        .cpu()
-        .numpy()
-    )
+    cosine_similarities = util.cos_sim(query_embeddings, node_embeddings).cpu().numpy()
     cosine_similarities_sorted = np.sort(cosine_similarities)
     percentile_A = np.percentile(cosine_similarities_sorted, range_percentile[0])
     percentile_B = np.percentile(cosine_similarities_sorted, range_percentile[1])
